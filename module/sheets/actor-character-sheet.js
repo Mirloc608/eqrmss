448e7c22439c891238f063d4cab8689cac92ad63
David Wruck <mirloc@gmail.com>
2026-09-25 13:18:24 -0400

    EQRMSS 1.06

--- module/sheets/actor-character-sheet.js
+++
@@ -1,3 +0,0 @@
-// Simple facade sheet modules for backward compatibility
-
-export { default } from "./actors/eqrmss_player_sheet.js";
--- module/sheets/actor-npc-sheet.js
+++
@@ -1,3 +0,0 @@
-// Simple facade NPC sheet module for backward compatibility
-
-export { default } from "./actors/eqrmss_npc_sheet.js";
